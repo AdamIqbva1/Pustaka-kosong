b1e351df4c9bdf30b28e2022227717954c7e766a
AdamIqva1 <adamiqbal167@gmail.com>
2022-03-25 14:41:57 +0700

    application/controllers/latihan1.php application/models/Model_latihan1.php application/views/view-latihan1.php

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,13 @@
+<html>
+
+<Head>
+    <title>Latihan 1</title>
+</Head>
+
+<body>
+    Halooo ayok belajar bareng gasss <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari penjumlahan <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+</body>
+</html>
